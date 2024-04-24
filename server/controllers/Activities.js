e1679f21ea73c821c90c453d15acd02cfa44d43c
Keith-Ocitti <83019128+Keith-Ocitti@users.noreply.github.com>
2024-04-24 15:10:23 +0300

    first commit

---
+++ server/controllers/Activities.js
@@ -0,0 +1,192 @@
+const Post = require("../models/Posts");
+const Room = require("../models/Room");
+const Resource = require("../models/Resource");
+const Event = require("../models/Events");
+const Comment = require("../models/Comments");
+const User = require("../models/User");
+const Message = require("../models/Message");
+const Clients = require("../models/Clients");
+
+const getTherapists = async (req, res) => {
+  const { userId } = req.user;
+  try {
+    let therapists = await User.find({ post: "therapist" });
+    let allMessages = await Message.find();
+    let result = therapists.map((therapist) => {
+      return {
+        therapistData: therapist,
+        messages: allMessages.filter(
+          (msg) =>
+            ([msg.to].toString() === [therapist._id].toString() &&
+              [msg.createdBy].toString() === userId) ||
+            ([msg.createdBy].toString() === [therapist._id].toString() &&
+              [msg.to].toString() === userId)
+        ),
+      };
+    });
+
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getClients = async (req, res) => {
+  try {
+    const { userId } = req.user;
+    console.log({ userId });
+    let allMessages = await Message.find();
+    const clients = await Clients.find({ therapistId: userId });
+    if (clients.length < 1) {
+      res.send([]);
+    }
+
+    let count = 1;
+    let result = clients.map((client) => {
+      // const clientData = await User.find({ _id: client.clientId });
+
+      return {
+        client: `Client ${count++}`,
+        id: client.clientId,
+        clientMessages: allMessages.filter(
+          (msg) =>
+            ([msg.to].toString() === userId &&
+              [msg.createdBy].toString() === [client.clientId].toString()) ||
+            ([msg.to].toString() === client.clientId &&
+              [msg.createdBy].toString() === userId)
+        ),
+      };
+    });
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+};
+
+const makePost = async (req, res, next) => {
+  try {
+    const { userId } = req.user;
+    const { body } = req.body;
+    const post = await Post.create({
+      body,
+      room: "65faef15f6903a53d3a10f40",
+      createdBy: userId,
+    });
+
+    res.status(201).send(post);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    const comments = await Comment.find();
+    let finalPosts = posts.map((post) => {
+      return {
+        post,
+        postComments: comments.filter(
+          (item) => [item.post].toString() === [post._id].toString()
+        ),
+      };
+    });
+    res.status(200).send(finalPosts);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+};
+
+const makeComment = async (req, res, next) => {
+  try {
+    const { userId } = req.user;
+    const { postId, body } = req.body;
+    const comment = await Comment.create({
+      body,
+      post: postId,
+      createdBy: userId,
+    });
+    res.status(201).send(comment);
+  } catch (error) {
+    res.send(error);
+    console.log(error);
+  }
+};
+
+const getComments = async (req, res, next) => {
+  try {
+    const { postId } = req.body;
+    const comments = await Comment.find({ post: postId });
+    res.status(201).send(comments);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+};
+
+const createResource = async (req, res, next) => {
+  try {
+    const { userId } = req.user;
+    const { title, body } = req.body;
+
+    await Resource.create({ title, body, createdBy: userId });
+    res.status(201).send("successful");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getResource = async (req, res) => {
+  try {
+    const resources = await Resource.find();
+    res.status(200).send(resources);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+};
+
+const postEvents = async (req, res, next) => {
+  try {
+    console.log("Server has been hit!");
+    const { userId } = req.user;
+    const { title, description, location, date, activityTime } = req.body;
+    console.log(req.body);
+    const event = await Event.create({
+      title,
+      description,
+      location,
+      createdBy: userId,
+      date,
+      activityTime,
+    });
+    res.status(201).send(event);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getEvents = async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.status(200).send(events);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+};
+
+module.exports = {
+  makePost,
+  makeComment,
+  createResource,
+  postEvents,
+  getPosts,
+  getTherapists,
+  getComments,
+  getClients,
+  getEvents,
+  getResource,
+};
